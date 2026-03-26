382983b21bdd3b6336060fc1a3e592e0c75588f5
Nguyễn Xuân Huỳnh <xuanhuynh543@gmail.com>
2026-03-26 13:40:13 +0700

    first commit

---
+++ src/main/java/com/webservice/be_tailflash/BeTailFlashApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.be_tailflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTailFlashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTailFlashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservice/be_tailflash/BeTailFlashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.be_tailflash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTailFlashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
